d6e930f89555b0f795953bd1bd4e3cadd9ade012
ouxiaofeng <1178445042@qq.com>
2024-09-26 17:00:40 +0800

    学习radiobutton

--- ui.py
+++ ui.py
@@ -4,27 +4,18 @@ window = tk.Tk() # 构造一个窗口对象
 window.title("my window")
 window.geometry("200x200")
 
-var1 = tk.StringVar()
-l = tk.Label(window,bg='yellow',width=4,textvariable=var1)
+var = tk.StringVar()
+l = tk.Label(window,bg='yellow',width=20,text='empty')
 l.pack()
 
 def print_selection():
-    value = lb.get(lb.curselection()) # 获取光标选定的东西
-    var1.set(value)
+    l.config(text='you have selectd ' + var.get()) # 修改label的参数
 
-
-b1 = tk.Button(window,text="print selection",width=15,height=2,command=print_selection)
-b1.pack()
-
-var2 = tk.StringVar()
-var2.set((11,22,33,44))
-lb = tk.Listbox(window,listvariable=var2)
-list_items = [1,2,3,4]
-for item in list_items:
-    lb.insert('end',item)
-lb.insert(1,'first') # 往列表的第一位插入first
-lb.insert(2,'second')
-lb.delete(2) # 删除列表的第二位
-lb.pack()
+r1 = tk.Radiobutton(window,text='Option A',variable=var,value='A',command=print_selection) # 选中时把 ’Option A‘改为’A‘ 
+r1.pack()
+r2 = tk.Radiobutton(window,text='Option B',variable=var,value='B',command=print_selection) # 选中时把 ’Option A‘改为’A‘ 
+r2.pack()
+r3 = tk.Radiobutton(window,text='Option C',variable=var,value='C',command=print_selection) # 选中时把 ’Option A‘改为’A‘ 
+r3.pack()
 
 window.mainloop() # 窗口不断刷新
