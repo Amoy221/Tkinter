95df2e63cd6936bff4fcd849be3f656cd50be7d3
ouxiaofeng <1178445042@qq.com>
2024-09-26 15:43:22 +0800

    本课学习了Label和Button

--- ui.py
+++ ui.py
@@ -1,1 +1,27 @@
+import tkinter as  tk
 
+window = tk.Tk() # 构造一个窗口对象 
+window.title("my window")
+window.geometry("200x100")
+
+var = tk.StringVar() # 定义一个字符串变量
+
+# l = tk.Label(window,text="va",bg='green',font=('Arial',12),width=15,height=2)
+l = tk.Label(window,textvariable=var,bg='green',font=('Arial',12),width=15,height=2) # 构造一个window上的Label对象
+l.pack()
+
+
+on_hit = False
+def hit_me():
+    global on_hit
+    if on_hit == False:
+        on_hit = True
+        var.set("you hit me")
+    else:
+        on_hit = False
+        var.set("")
+
+b = tk.Button(window,text="hit me",width=15,height=2,command=hit_me)
+b.pack()
+
+window.mainloop() # 窗口不断刷新
