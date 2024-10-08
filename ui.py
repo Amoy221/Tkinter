09039a2c4358f60c97e870610ffc97d6c4bdb24c
ouxiaofeng <1178445042@qq.com>
2024-10-08 11:30:48 +0800

    menubar菜单

--- ui.py
+++ ui.py
@@ -2,22 +2,37 @@ import tkinter as  tk
 
 window = tk.Tk() # 构造一个窗口对象 
 window.title("my window")
-window.geometry("800x600")
+window.geometry("200x200")
 
-canvas = tk.Canvas(window,bg = 'blue',height=300,width=300)
-canvas.pack()
-image_file = tk.PhotoImage(file='D:\MyPython\Tkinter\dog.gif') # 打开image文件,tkinter支持gif格式的图片
-image = canvas.create_image(10,10,anchor='nw',image=image_file) # 0,,0是是指画布中的哪个点，anchor是锚定点
-x0,y0,x1,y1=50,50,80,80 # 自定义坐标
-line = canvas.create_line(x0,y0,x1,y1) # 画一根黑线
-oval = canvas.create_oval(x0,y0,x1,y1,fill='red') # 画圆形
-arc = canvas.create_arc(x0+30,y0+30,x1+30,y1+30,start = 0,extent=180) # 画扇形，从0打开到180收回  
-rect = canvas.create_rectangle(100,30,100+20,30+20) 
+l = tk.Label(window,text="",bg='yellow')
+l.pack()
 
-def moveit(): # 点击button，，正方形Y轴方向下移两个单位
-    canvas.move(rect,0,2)  # 0是X坐标0个单位，2是Y坐标两个单位
+counter = 0
+def do_job():
+    global counter
+    l.config(text='do'+str(counter)) # 改变label的参数
+    counter+=1
 
-b = tk.Button(window,text='move',command=moveit).pack()
+menubar = tk.Menu(window)
+filemenu =  tk.Menu(menubar,tearoff=0) # 定义在菜单栏中的menu,tearoff=0可以把虚线去除
+menubar.add_cascade(label='File',menu=filemenu) # 显示在menubar上面
+filemenu.add_command(label='New',command=do_job) # 定义选项功能
+filemenu.add_command(label='Open',command=do_job)
+filemenu.add_command(label='Save',command=do_job)
 
-                    
+filemenu.add_separator()
+filemenu.add_command(label='Exit',command=window.quit)
+
+Editmenu =  tk.Menu(menubar,tearoff=0)
+menubar.add_cascade(label='Edit',menu=Editmenu)
+Editmenu.add_command(label='Cut',command=do_job)
+Editmenu.add_command(label='Copy',command=do_job)
+Editmenu.add_command(label='Paste',command=do_job)
+
+submenu = tk.Menu(filemenu,tearoff=0)
+filemenu.add_cascade(label='Import',menu=submenu,underline=0)
+submenu.add_command(label='Submenu1',command=do_job)
+
+window.config(menu=menubar) # 改变window的参数，把菜单栏放在window上    
+
 window.mainloop() # 窗口不断刷新
