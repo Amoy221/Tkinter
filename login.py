080efb40f0ebb91c5812277f61f659cea00c0172
ouxiaofeng <1178445042@qq.com>
2024-10-08 16:04:12 +0800

    登陆窗口第2课

--- login.py
+++ login.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import tkinter as tk
+import pickle
+from tkinter import messagebox
 
 window = tk.Tk()
 window.title("Welcome to MyPython")
-window.geometry('450x300')
+window.geometry('450x400')
 
 # image
 canvas = tk.Canvas(window,height=200,width=500)
@@ -26,7 +28,25 @@ entry_usr_pwd.place(x=160,y=240)
 
 # login and sign up button
 def usr_login():
-    pass
+    usr_name = var_usr_name.get()
+    usr_pwd = var_usr_pwd.get()
+    try:
+        with open('user_info.pickle','rb') as usr_file:
+            usrs_info = pickle.load(usr_file)
+    except FileNotFoundError:
+        with open('usrs_info.pickle','wb') as usr_file:
+            usrs_info = {"admin":"admin"}
+            pickle.dump(usrs_info,usr_file)
+
+    if usr_name in  usrs_info:
+        if usr_pwd == usrs_info[usr_name]:
+            tk.messagebox.showinfo(title='Welcome',message='How are you?'+ usr_name)
+        else:
+            tk.messagebox.showerror(message='Error,your password is wrong,try again.')
+    else:
+        is_sign_up = tk.messagebox.askyesno('Welcome','You have not sign up yet.Sign up today?')
+        if is_sign_up:
+            usr_sign_up()
 
 def usr_sign_up():
     pass
