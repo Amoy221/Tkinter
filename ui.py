f66d83301b315ec56621f76e8662a162bb137ea0
ouxiaofeng <1178445042@qq.com>
2024-10-08 10:54:43 +0800

    学习canvas画布

--- ui.py
+++ ui.py
@@ -2,27 +2,22 @@ import tkinter as  tk
 
 window = tk.Tk() # 构造一个窗口对象 
 window.title("my window")
-window.geometry("200x200")
+window.geometry("800x600")
 
+canvas = tk.Canvas(window,bg = 'blue',height=300,width=300)
+canvas.pack()
+image_file = tk.PhotoImage(file='D:\MyPython\Tkinter\dog.gif') # 打开image文件,tkinter支持gif格式的图片
+image = canvas.create_image(10,10,anchor='nw',image=image_file) # 0,,0是是指画布中的哪个点，anchor是锚定点
+x0,y0,x1,y1=50,50,80,80 # 自定义坐标
+line = canvas.create_line(x0,y0,x1,y1) # 画一根黑线
+oval = canvas.create_oval(x0,y0,x1,y1,fill='red') # 画圆形
+arc = canvas.create_arc(x0+30,y0+30,x1+30,y1+30,start = 0,extent=180) # 画扇形，从0打开到180收回  
+rect = canvas.create_rectangle(100,30,100+20,30+20) 
 
-l = tk.Label(window,bg='yellow',width=20,text='empty')
-l.pack()
+def moveit(): # 点击button，，正方形Y轴方向下移两个单位
+    canvas.move(rect,0,2)  # 0是X坐标0个单位，2是Y坐标两个单位
 
-def print_selection(): 
-    if (var1.get()==1) & (var2.get()==0):
-        l.config(text='I love only python ') # 修改label的参数
-    elif (var1.get()==0) & (var2.get()==1):
-        l.config(text='I love only C++ ')
-    elif (var1.get()==0) & (var2.get()==0):
-        l.config(text='I do not love either ')
-    else:
-        l.config(text='I love both ')
+b = tk.Button(window,text='move',command=moveit).pack()
 
-var1 = tk.IntVar()
-var2 = tk.IntVar()
-c1 = tk.Checkbutton(window,text='python',variable=var1,onvalue=1,offvalue=0,command=print_selection) # 选定时，把onvalue赋值给var1
-c2 = tk.Checkbutton(window,text='c++',variable=var2,onvalue=1,offvalue=0,command=print_selection)
-c1.pack()
-c2.pack()
                     
 window.mainloop() # 窗口不断刷新
