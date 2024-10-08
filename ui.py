7f45dbfba040936385414e3f441084c8898691c3
ouxiaofeng <1178445042@qq.com>
2024-10-08 14:42:57 +0800

    messagebox弹窗

--- ui.py
+++ ui.py
@@ -1,19 +1,20 @@
 import tkinter as  tk
+from tkinter import messagebox
 
 window = tk.Tk() # 构造一个窗口对象 
 window.title("my window")
 window.geometry("200x200")
-tk.Label(window,text='on the window').pack()
 
-frm = tk.Frame(window)
-frm.pack()
-frm_l = tk.Frame(frm,)
-frm_r = tk.Frame(frm,)
-frm_l.pack(side='left')
-frm_r.pack(side='right')
+def hit_me():
+    # tk.messagebox.showinfo(title='Hi',message='hahahha')
+    # tk.messagebox.showwarning(title='Hi',message='nononono') # 警告
+    # tk.messagebox.showerror(title='Hi',message='NO! never')   # 报错
+    # print(tk.messagebox.askquestion(title='Hi',message='hahahahh'))  # return 'yes','no'
+    # print(tk.messagebox.askyesno(title='Hi',message='hahahaha')) # return 'true','false'
+    # print(tk.messagebox.askretrycancel(title='Hi',message='hahahaha')) # return 'true','false'
+    print(tk.messagebox.askokcancel(title='Hi',message='hahahaha')) # return 'true','false'
 
-tk.Label(frm_l,text='on the frm_l1').pack()
-tk.Label(frm_l,text='on the frm_l2').pack()
-tk.Label(frm_r,text='on the frm_r1').pack()
+
+tk.Button(window,text='hit me',command=hit_me).pack()
 
 window.mainloop() # 窗口不断刷新
