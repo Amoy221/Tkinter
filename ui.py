a228018f0a5cf839baeae7e4211731fe8ab6b12d
ouxiaofeng <1178445042@qq.com>
2024-10-08 14:57:43 +0800

    pack、grid、place放置位置

--- ui.py
+++ ui.py
@@ -5,16 +5,16 @@ window = tk.Tk() # 构造一个窗口对象
 window.title("my window")
 window.geometry("200x200")
 
-def hit_me():
-    # tk.messagebox.showinfo(title='Hi',message='hahahha')
-    # tk.messagebox.showwarning(title='Hi',message='nononono') # 警告
-    # tk.messagebox.showerror(title='Hi',message='NO! never')   # 报错
-    # print(tk.messagebox.askquestion(title='Hi',message='hahahahh'))  # return 'yes','no'
-    # print(tk.messagebox.askyesno(title='Hi',message='hahahaha')) # return 'true','false'
-    # print(tk.messagebox.askretrycancel(title='Hi',message='hahahaha')) # return 'true','false'
-    print(tk.messagebox.askokcancel(title='Hi',message='hahahaha')) # return 'true','false'
+# tk.Label(window,text=1).pack(side='top')
+# tk.Label(window,text=1).pack(side='bottom')
+# tk.Label(window,text=1).pack(side='left')
+# tk.Label(window,text=1).pack(side='right')
 
+# for i in range(4):
+#     for j in range(3):
+#         tk.Label(window,text=1).grid(row=i,column=j,padx=10,pady=10)
 
-tk.Button(window,text='hit me',command=hit_me).pack()
+tk.Label(window,text=1).place(x=10,y=100,anchor='nw') # anchor锚点，用于将图片左上角定于（10，100）
+
 
 window.mainloop() # 窗口不断刷新
